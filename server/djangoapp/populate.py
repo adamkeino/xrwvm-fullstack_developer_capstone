df0c6e4c3c022ff889859ce4bfb26e3dfca91c33
Adam Keino <keinoadamdev@gmail.com>
2024-03-19 13:46:44 +0300

    Python lint test

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -12,13 +12,14 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'],
-            description=data['description']))
+        car_make_instances.append(CarMake.objects.create
+                                  (name=data['name'],
+                                   description=data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
       {"name": "Pathfinder", "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[0]},
+       "car_make": car_make_instances[0]},
       {"name": "Qashqai", "type": "SUV", "year": 2023,
        "car_make": car_make_instances[0]},
       {"name": "XTRAIL", "type": "SUV", "year": 2023,
@@ -51,5 +52,7 @@ def initiate():
     ]
 
     for data in car_model_data:
-        CarModel.objects.create(name=data['name'], car_make=data['car_make'],
-         type=data['type'], year=data['year'])
+        CarModel.objects.create(name=data['name'],
+                                car_make=data['car_make'],
+                                type=data['type'],
+                                year=data['year'])
